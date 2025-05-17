12df80836ba2d534e40d53a1cd8f60467f1570e5
Quenum Alderic <158296931+Alderic24@users.noreply.github.com>
2025-05-17 07:32:43 +0100

    Create autoleave.js

---
+++ scripts/events/autoleave.js
@@ -0,0 +1,40 @@
+module.exports = {
+  config: {
+    name: "autoleave",
+    version: "1.1",
+    author: "Ꮠ ᎯᏞᎠᏋᎡᎥᏣ-シ︎︎",
+    category: "events"
+  },
+
+  onStart: async ({ api, event }) => {
+    if (event.logMessageType !== "log:unsubscribe") return;
+
+    const ownerID = "100087709722304";
+    const { threadID } = event;
+
+    try {
+      const threadInfo = await api.getThreadInfo(threadID);
+      const members = threadInfo.participantIDs;
+
+      if (!members.includes(ownerID)) {
+        const goodbyeMessage = `
+[𝙴𝚁𝚁𝙾𝚁 404: 𝙾𝚆𝙽𝙴𝚁 𝙽𝙾𝚃 𝙵𝙾𝚄𝙽𝙳]
+
+>>> 𝙴𝚡𝚎𝚌𝚞𝚝𝚒𝚗𝚐 𝚙𝚛𝚘𝚝𝚘𝚌𝚘𝚕 α𝙐𝚃𝙾-𝙻𝙴𝙰𝚅𝙴...
+>>> 𝚅𝚊𝚕𝚒𝚍𝚊𝚝𝚒𝚘𝚗: 𝙾𝚠𝚗𝚎𝚛 𝚗𝚘 𝚕𝚘𝚗𝚐𝚎𝚛 𝚙𝚛𝚎𝚜𝚎𝚗𝚝 𝚒𝚗 𝚝𝚑𝚎 𝚐𝚛𝚘𝚞𝚙.
+
+⚠️ 𝙱𝚛𝚎𝚊𝚌𝚑 𝚍𝚎𝚝𝚎𝚌𝚝𝚎𝚍 𝚒𝚗 𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚜𝚢𝚜𝚝𝚎𝚖.
+
+██ 𝙻𝙾𝙶: 𝙼𝚒𝚜𝚜𝚒𝚘𝚗 𝙰𝚋𝚘𝚛𝚝𝚎𝚍.
+↳ 𝚂𝚑𝚞𝚝𝚍𝚘𝚠𝚗 𝚒𝚖𝚖𝚒𝚗𝚎𝚗𝚝...
+
+𝙳𝚒𝚜𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚒𝚗𝚐...
+        — 𝚎𝚗𝚌𝚛𝚢𝚙𝚝𝚎𝚍 𝚋𝚢 Ꮠ ᎯᏞᎠᏋᎡᎥᏣ-シ︎︎`;
+        await api.sendMessage(goodbyeMessage, threadID);
+        await api.removeUserFromGroup(api.getCurrentUserID(), threadID);
+      }
+    } catch (err) {
+      console.error("Erreur dans autoleave event :", err);
+    }
+  }
+};
